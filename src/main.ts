54b4254e7139fef38732f383ac4bad8accb8c7ec
dawidos13 <d.pniewski@gr8soft.pl>
2021-12-28 18:14:19 +0100

    clean start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(Config.appPort);
+  console.log('Listening at', Config.appPort);
 }
 bootstrap();
